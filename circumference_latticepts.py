11d1934e8e60cabde32945192fb0d7e94d5da7a1
Craig Chen <craig.chen@duke.edu>
2020-06-25 16:23:44 -0700

    angles between lattice points of upper right octant

--- circumference_latticepts.py
+++ circumference_latticepts.py
@@ -1,15 +1,22 @@
 import math
 import gmpy2
 
-radius = 1000000
+radius = 10
+max_ang = 1/math.sqrt(radius)
 
 # find lattice points
 
 lp = [(0,radius)]
 
-for i in range(int(radius//math.sqrt(2)),radius):
+for i in reversed(range(int(radius//math.sqrt(2)),radius)):
     cand = radius**2 - i**2
     if gmpy2.is_square(cand):
         lp.append((math.sqrt(cand),i))
         # print(i)
+
+angles = []
+for j in range(len(lp)-1):
+    angles.append( math.atan2(*lp[j][::-1])-math.atan2(*lp[j+1][::-1]) )
     
+angles.append( math.atan2(*lp[-1][::-1]) - math.pi/4 )
+
