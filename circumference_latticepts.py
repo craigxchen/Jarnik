4e3f59f94ac5bcbfd70acad8046903293ed65f73
Craig Chen <craig.chen@duke.edu>
2020-06-26 16:46:22 -0700

    counting lattice points works, may be slow for large R

--- circumference_latticepts.py
+++ circumference_latticepts.py
@@ -1,8 +1,10 @@
 import math
 import gmpy2
 
-radius = 10
-max_ang = 1/math.sqrt(radius)
+epsilon = 0.01
+
+radius = 25
+max_ang = 16**(1/3) * radius**(-0.5+epsilon)
 
 # find lattice points
 
@@ -19,4 +21,19 @@ for j in range(len(lp)-1):
     angles.append( math.atan2(*lp[j][::-1])-math.atan2(*lp[j+1][::-1]) )
     
 angles.append( math.atan2(*lp[-1][::-1]) - math.pi/4 )
+# angles[0:0] = angles[::-1]
 
+count = []
+for i in range(len(lp)):
+    # check how many lattice points would fit in arc of length radius^0.5
+    running_sum = 0
+    running_count = 1
+
+    for ang in angles[i:]:
+        running_sum += ang
+        if running_sum > max_ang:
+            break
+        running_count += 1
+         
+    count.append(running_count)
+        
