0c66cfdcb6b5e8ebe0daf9c604889634730e86aa
Craig Chen <craigxchen@gmail.com>
2020-09-01 15:42:43 -0400

    fixed error causing overcounting for integer R^2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
-extern crate csv;
+// extern crate csv;
 
 use std::collections::HashMap;
 use std::vec::Vec;
 use std::f64::consts::PI;
 use std::error;
-use csv::Writer;
+// use csv::Writer;
 
 const BETA: f64 = 0.5;
-const MIN_R: u64 = 10;
-const MAX_R: u64 = 10000;
+const MIN_R: u64 = 5695325;
+const MAX_R: u64 = 5695325;
 const VALID_RESIDUALS: [u64; 5] = [1,4,5,6,9];
 
 fn main() -> Result<(), Box<dyn error::Error>>{
    let res = get_max_lattice_points(MIN_R, MAX_R);
 
     let max_count = res.values().max().unwrap();
+    let mut max_rads = find_keys_for_value(&res,max_count);
+    max_rads.sort();
 
     println!("From radius = {} to {}", MIN_R, MAX_R);
     println!("The maximum number of lattice points for beta = {} is {:?}", BETA, max_count);
+    println!("The corresponding squared radii are {:?}", max_rads);
 
     // let mut wtr = Writer::from_path("jarnik1to10.csv")?;
     // wtr.write_record(&["Radius", "Max Lattice Points"])?;
@@ -28,12 +31,17 @@ fn main() -> Result<(), Box<dyn error::Error>>{
     Ok(())
 }
 
+fn find_keys_for_value<'a>(map: &'a HashMap<u64,u64>, value: &u64) -> Vec<&'a u64> {
+    map.iter()
+        .filter_map(|(key, val)| if val == value { Some(key) } else { None })
+        .collect()
+}
+
 fn get_max_lattice_points<'a>(min_r: u64, max_r: u64) -> HashMap<u64, u64> {
     let mut rad_counts: HashMap<u64,u64> = HashMap::new();
 
-    for radius in min_r..=max_r {
-        let radius = radius as f64;
-        // let radius = (radius as f64).sqrt();
+    for R in min_r..=max_r {
+        let radius = (R as f64).sqrt();
 
         let mut lattice_points: Vec<(f64, f64)> = Vec::new();
         let mut angles: Vec<f64> = Vec::new();
@@ -52,8 +60,11 @@ fn get_max_lattice_points<'a>(min_r: u64, max_r: u64) -> HashMap<u64, u64> {
                 }
             }
         }
+        // println!{"{:?}",lattice_points};
         let lp_len = lattice_points.len();
         if lp_len > 0 {
+            angles.push(2.0 * lattice_points[0].1.atan2(0.0));
+
             for i in 0..(lp_len-1) {
                 let (x1, y1) = lattice_points[i];
                 let (x2, y2) = lattice_points[i + 1];
@@ -62,7 +73,7 @@ fn get_max_lattice_points<'a>(min_r: u64, max_r: u64) -> HashMap<u64, u64> {
                 angles.push(a1 - a2)
             }
         } else {
-            rad_counts.insert(radius as u64, 0);
+            rad_counts.insert(R, 0);
             continue
         }
 
@@ -83,7 +94,7 @@ fn get_max_lattice_points<'a>(min_r: u64, max_r: u64) -> HashMap<u64, u64> {
 
             for ang in &angles[i..] {
                 running_ang_sum += *ang;
-                if running_ang_sum > max_ang {
+                if running_ang_sum >= max_ang {
                     break
                 }
                 running_count += 1;
@@ -92,7 +103,7 @@ fn get_max_lattice_points<'a>(min_r: u64, max_r: u64) -> HashMap<u64, u64> {
                 count = running_count;
             }
         }
-        rad_counts.insert(radius as u64, count);
+        rad_counts.insert(R, count);
     }
     rad_counts
 }
