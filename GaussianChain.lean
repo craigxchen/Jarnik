2c2fbfb0ad999bdd8b7b2c58086a8995876b8d12
Craig Chen <33159397+craigxchen@users.noreply.github.com>
2026-07-28 19:52:21 -0400

    Import uniform GCD formalization modules

--- GaussianChain.lean
+++ GaussianChain.lean
@@ -26,3 +26,5 @@ import GaussianChain.Certificate
 import GaussianChain.Extension
 import GaussianChain.Discriminant
 import GaussianChain.Examples
+import GaussianChain.FiniteBeta
+import GaussianChain.UniformExceptionalReduction
